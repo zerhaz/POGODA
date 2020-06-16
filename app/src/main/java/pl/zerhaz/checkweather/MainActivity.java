a5865dc431b6db3de39d542ea24b4f7a819c8af2
Zerhaz <m.stancelewski@gmail.com>
2020-06-16 20:53:48 +0200

    Final commit

--- app/src/main/java/pl/zerhaz/checkweather/MainActivity.java
+++ app/src/main/java/pl/zerhaz/checkweather/MainActivity.java
@@ -5,7 +5,6 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.content.SharedPreferences;
-import android.content.res.Resources;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
